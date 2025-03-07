5f13171a2fc5c49a706a0beb64a957e3f3e8a647
erdogant <erdogant@gmail.com>
2025-03-07 13:03:23 +0100

    update examples

--- ffmpeg_setpath/examples.py
+++ ffmpeg_setpath/examples.py
@@ -8,15 +8,20 @@ from ffmpeg_setpath import ffmpeg_setpath
 # ffmpeg_setpath(dirpath=r'c:/ffmpeg/')
 ffmpeg_setpath()
 
+# %% Check path
+import ffmpeg_setpath as ff
+ff.check(r'c:/ffmpeg/bin', exact_match=True)
+ff.check(r'ffmpeg', exact_match=False)
+
 # %% Force to set path in env
-import ffmpeg_setpath
-ffmpeg_setpath.set_path(r'c:/temp/ffmpeg/')
+import ffmpeg_setpath as ff
+ff.set_path(r'c:/temp/ffmpeg/')
 
 # %% Remove specified path from env
-import ffmpeg_setpath
-ffmpeg_setpath.remove(r'c:\ffmpeg\bin')
+import ffmpeg_setpath as ff
+ff.remove(r'c:\ffmpeg\bin')
 
 # %% Show all paths in env
-import ffmpeg_setpath
-ffmpeg_setpath.printe()
+import ffmpeg_setpath as ff
+ff.printe()
 
